b5983acd79226f4326ada9fa9387ecb71b0b7a9f
Chanudhi <aluchanu29@gmail.com>
2025-05-09 16:54:26 +0530

    Refactor Navbar component to use a links array and improve layout styling

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,14 +1,22 @@
 import Link from 'next/link'
 import React from 'react'
+import { AiFillBug } from "react-icons/ai";
 
 const Navbar = () => {
+    const links = [
+        {label: 'Dashboard', href: '/'},
+        {label: 'Issues', href: '/issues'},
+    ]
   return (
-    <nav className='flex space-x-9 border-b mb-5 px-5 h14 items-center'>
-        <Link href='/'>Logo </Link>
+    <nav className='flex space-x-9 border-b mb-5 px-8 h24 items-center text-3xl'>
+        <Link href='/'><AiFillBug/> </Link>
         <ul className='flex space-x-6'>
-            <li><Link href='/'>Dashboard</Link></li>
-            <li><Link href='/issues'>Issues</Link></li>
-        </ul>
+            {links.map(links =>(
+                <Link key={links.href}
+                 className='text-zinc-500 hover:text-zinc-800 transition-colors' href={links.href}>{links.label}
+                </Link>
+            ))}
+            </ul>
     </nav>
 
   )
