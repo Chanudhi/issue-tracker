b68359cdccc4434d1a3891b50af6edecec452294
Chanudhi <aluchanu29@gmail.com>
2025-05-09 20:22:45 +0530

    Enhance Navbar component with active link styling and add Issues page

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,19 +1,24 @@
+'use client';
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation';
 import { AiFillBug } from "react-icons/ai";
+import classnames from
 
 const Navbar = () => {
+    const currentPath = usePathname();
+
     const links = [
         {label: 'Dashboard', href: '/'},
         {label: 'Issues', href: '/issues'},
     ]
   return (
-    <nav className='flex space-x-9 border-b mb-5 px-8 h24 items-center text-3xl'>
+    <nav className='flex space-x-9 border-b mb-5 px-8 h-14 items-center text-xl'>
         <Link href='/'><AiFillBug/> </Link>
-        <ul className='flex space-x-6'>
+        <ul className='flex space-x-6 ml-6'>
             {links.map(links =>(
                 <Link key={links.href}
-                 className='text-zinc-500 hover:text-zinc-800 transition-colors' href={links.href}>{links.label}
+                 className={`${links.href==currentPath ? 'text-zinc-900':'text-zinc-500'} hover:text-zinc-800 transition-colors`} href={links.href}>{links.label}
                 </Link>
             ))}
             </ul>
